a587cf54f646cd9783e6b6923f9bb57e6f6cab33
higor barbosa <azurerepos@gmail.com>
2020-12-12 11:27:38 -0300

    Application Settings no App Service.

---
+++ Modulo2/ApplicationSettings/Controllers/AppSettingsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace ApplicationSettings.Controllers
+{
+	[ApiController]
+	[Route("[controller]")]
+	public class AppSettingsController : ControllerBase
+	{
+		private readonly IConfiguration Configuration;
+		public AppSettingsController(IConfiguration configuration)
+		{
+			Configuration = configuration;
+
+		}
+		[HttpGet]
+		public string Get()
+		{
+			return Configuration["Chave"].ToString();
+		}
+	}
+}
